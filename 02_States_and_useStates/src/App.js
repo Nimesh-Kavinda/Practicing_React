d75da11084b594d86c7f09edbefa703dfb31379d
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-18 15:48:38 +0530

    Learn about useState Multiple Updates

--- 02_States_and_useStates/src/App.js
+++ 02_States_and_useStates/src/App.js
@@ -6,8 +6,9 @@ function App() {
   const [count, setCount] = useState(0);
 
   function handleADD(){
-    setCount(count + 1);
-    
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+    setCount(count => count + 1);
   }
 
   function handleSUB(){
