5841db0c6df91f9dfea621dfd8b85555cf444477
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-18 14:42:39 +0530

    Learn more about state and useState

--- 02_States_and_useStates/src/App.js
+++ 02_States_and_useStates/src/App.js
@@ -14,12 +14,17 @@ function App() {
     setCount(count - 1);
   }
 
+  function handleRESET(){
+    setCount(0);
+  }
+
   return (
     <div className="App">
      <div className="box">
       <p>{count}</p>
       <button onClick ={handleADD} className='add'>ADD</button>
       <button onClick ={handleSUB}  className="sub">SUB</button>
+      <button onClick ={handleRESET}  className="reset">RESET</button>
      </div>
     </div>
   );
