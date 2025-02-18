2a391d9a2c4ec79d980a065a3ad6d54d98f58506
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-18 14:16:36 +0530

    Learned Why we need States

--- 02_States_and_useStates/src/App.js
+++ 02_States_and_useStates/src/App.js
@@ -2,9 +2,21 @@
 import './App.css';
 
 function App() {
+
+  let count = 0;
+
+  function handleADD(){
+    count += 1;
+    console.log(count);
+  }
+
   return (
-    <div>
-     
+    <div className="App">
+     <div className="box">
+      <p>{count}</p>
+      <button onClick ={handleADD} className='add'>ADD</button>
+      <button className="sub">SUB</button>
+     </div>
     </div>
   );
 }
