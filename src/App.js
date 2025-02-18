2342a816d5b3f235b40173a58a86834b187d8028
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-18 12:27:05 +0530

    Learned about Inline Dynamic Content (Using js in JSX with varibels and also loops and others)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 
 export default function App() {
+
+  const username = "Nimesh";
+
   return (
      <>
-          <h1 className='active'>Nimesh</h1>
+          <h1 className='active'>{username}</h1>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, iusto.</p>
           <img src="" alt="" />
      </>
