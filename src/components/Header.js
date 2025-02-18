e64186d803b2416652e9b92789b594e5da1d2fed
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-18 13:30:58 +0530

    Learned About How to Add Images and Howt use Css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
-
+import Logo from "../assets/logo.png"
+import './header.css'
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <img className="logo" src={Logo} alt="" />
+    </div>
   )
 }
