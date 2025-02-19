53803ca62aa15aede89bbf47e3dd87ae3b7b88a8
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-19 12:19:49 +0530

    Learned about multiple components fragments and get an introduction about Props

--- 03_Components_Props/src/App.js
+++ 03_Components_Props/src/App.js
@@ -1,50 +1,13 @@
-import { useState } from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { TaskList } from './components/TaskList';
 
 function App() {
 
-  const [tasks, setTasks] = useState([
-    {
-      id : 5271,
-      name : "Record Lectures",
-      completed : false
-    }, 
-
-    {
-      id : 7825,
-      name : "Edit React Lectures",
-      completed : true
-    }, 
-    
-    {
-      id : 8391,
-      name : "Watch Leacturs",
-      completed : false
-    }
-  ]);
-
-  const [show , setShow] = useState(true);
-
-  function handleDelete(id){
-    setTasks(tasks.filter(task => id !== task.id));
-  }
-
   return (
     <div className="App">
-    <h1>Task List</h1>
-    <ul>
-        <button className='trigger' onClick={() => setShow(!show)}>Toggle</button>
-
-      {show && tasks.map((task) => (
-        
-        <li key={task.id} className={task.completed ? "completed" : "incomplete"}>
-          <span>{task.id} - {task.name}</span>
-          <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
-        </li>
-        
-      ))}
-
-    </ul>
+    <Header/>
+    <TaskList title = "Random" subtitle = "Test"/>
     </div>
   );
 }
