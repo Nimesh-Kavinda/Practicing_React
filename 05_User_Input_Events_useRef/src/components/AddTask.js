307b9aaa7bc7b942ea0f664fbf1e922f995144ea
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-03 21:51:48 +0530

    COntrolled input using reset onClick

--- 05_User_Input_Events_useRef/src/components/AddTask.js
+++ 05_User_Input_Events_useRef/src/components/AddTask.js
@@ -5,13 +5,18 @@ export const AddTask = () => {
 
     const [taskValue, setTaskValue] = useState("");
 
+    const handleReset = () => {
+        setTaskValue("");
+    }
+
   return (
     <section className="addtask">
         <form>
-            <input onChange={(e) =>  setTaskValue(e.target.value)} type="text" name= "task" id = "task" placeholder="Task Name" autoComplete="off"/>
+            <input onChange={(e) =>  setTaskValue(e.target.value)} type="text" name= "task" id = "task" placeholder="Task Name" autoComplete="off" value={taskValue}/>
             <button type="submit">Add Task</button>
+            <span onClick={handleReset} className="reset">Reset</span>
         </form>
-        <p>{taskValue.length}</p>
+        <p>{taskValue}</p>
     </section>
   )
 }
