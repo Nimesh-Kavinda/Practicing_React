e991673404f7a59e632210fefd3e931f9b01267e
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-03 21:30:41 +0530

    Learn about OnChnage Events

--- 05_User_Input_Events_useRef/src/components/AddTask.js
+++ 05_User_Input_Events_useRef/src/components/AddTask.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import "./AddTask.css";
 
 export const AddTask = () => {
+
+    const [taskValue, setTaskValue] = useState("");
+
   return (
     <section className="addtask">
         <form>
-            <label htmlFor="task">Task Name : </label>
-            <input type="text" name= "task" id = "task" placeholder="Type here Task Name" autoComplete="off"/>
+            <input onChange={(e) =>  setTaskValue(e.target.value)} type="text" name= "task" id = "task" placeholder="Task Name" autoComplete="off"/>
             <button type="submit">Add Task</button>
         </form>
+        <p>{taskValue.length}</p>
     </section>
   )
 }
