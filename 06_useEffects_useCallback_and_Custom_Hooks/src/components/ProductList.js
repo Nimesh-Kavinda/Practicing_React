cdcff38a3a3a2f7310436bb575d320afd066f257
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-03 22:24:54 +0530

    Learned about useCallback Hook

--- 06_useEffects_useCallback_and_Custom_Hooks/src/components/ProductList.js
+++ 06_useEffects_useCallback_and_Custom_Hooks/src/components/ProductList.js
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 export const ProductList = () => {
 
     const [products, setProducts] = useState([]);
     const [url, setUrl] = useState("http://localhost:8000/products");
-    const [counter, setCounter] = useState(0);
-   
-    useEffect(() => {
-      fetch(url)
-      .then(response => response.json())
-      .then(data => setProducts(data) );
-    }, [url]) 
 
+    
+    const fetchProducts = useCallback(  async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setProducts(data);
+    }, [url])
+    
     useEffect(() => {
-        console.log(counter);
-    }, [counter])
+      fetchProducts();
+      console.log("-");
+    }, [fetchProducts]); 
+
 
   return (
     <section>
     <div className="filter">
-        <button onClick={() => setCounter(counter + 1)}>{counter}</button>
         <button onClick={() => setUrl("http://localhost:8000/products")}>All</button>
         <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock</button>
         <button onClick={() => setUrl("http://localhost:8000/products?in_stock=false")}>Unavailable</button>
