5b839d0ef6d2ad415d243e230865114b5840367b
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-03 22:39:48 +0530

    Learn How to create and use Custom hooks

--- 06_useEffects_useCallback_and_Custom_Hooks/src/components/ProductList.js
+++ 06_useEffects_useCallback_and_Custom_Hooks/src/components/ProductList.js
@@ -1,22 +1,11 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, {  useState } from 'react'
+import { useFetch } from '../hooks/useFetch';
 
 export const ProductList = () => {
 
-    const [products, setProducts] = useState([]);
     const [url, setUrl] = useState("http://localhost:8000/products");
-
+    const {data : products} = useFetch(url);
     
-    const fetchProducts = useCallback(  async () => {
-      const response = await fetch(url);
-      const data = await response.json();
-      setProducts(data);
-    }, [url])
-    
-    useEffect(() => {
-      fetchProducts();
-      console.log("-");
-    }, [fetchProducts]); 
-
 
   return (
     <section>
@@ -25,7 +14,7 @@ export const ProductList = () => {
         <button onClick={() => setUrl("http://localhost:8000/products?in_stock=true")}>In Stock</button>
         <button onClick={() => setUrl("http://localhost:8000/products?in_stock=false")}>Unavailable</button>
     </div>
-        { products.map((product) => (
+        { products && products.map((product) => (
             <div className="card" key={product.id}>
             <p className='id'>{product.id}</p>
             <p className='name'>{product.name}</p>
