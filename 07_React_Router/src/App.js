36ea755c7a7564c73a427e833974dc1648a39105
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-06-14 23:55:46 +0530

    Make the Header and Footer Components

--- 07_React_Router/src/App.js
+++ 07_React_Router/src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
+import {Header} from './components/Header';
+import { Footer } from './components/Footer';
 import { Home } from './components/Home';
 import { ProductList } from './components/ProductList';
 import { ProductDetail } from './components/ProductDetail';
@@ -10,14 +12,18 @@ import './App.css';
 function App() {
   return (
       <div className="App">
-      <header>Header</header>
+
+      <Header/>
+
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='products' element={<ProductList/>}></Route>
         <Route path='products/1001' element={<ProductDetail/>}></Route>
         <Route path='contact' element={<Contact/>}></Route>
       </Routes>
-      <footer>Footer</footer>
+      
+      <Footer/>
+      
     </div>
   );
 }
