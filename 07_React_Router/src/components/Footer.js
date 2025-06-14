919d75e1217ba62565dc136f5ae021ffa835ddf8
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-06-15 00:14:48 +0530

    Try the Link tag with also with footer

--- 07_React_Router/src/components/Footer.js
+++ 07_React_Router/src/components/Footer.js
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
+
 export const Footer = () => {
   return (
     <footer>
-      2025 RouteMate
+    <Link to="/"> 2025 RouteMate</Link>
     </footer>
   )
 }
