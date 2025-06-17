66c9ed2004adc1cb59866d52d04b9a8d0546ac79
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-06-17 23:39:01 +0530

    Learned About serach Parameters with useSearchparams

--- 07_React_Router/src/components/ProductList.js
+++ 07_React_Router/src/components/ProductList.js
@@ -1,4 +1,11 @@
+import { useSearchParams } from "react-router-dom";
+
 export const ProductList = () => {
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get('keyword'));
+  console.log(searchParams.get('instock'));
+  console.log(searchParams.get('rating'));
+
   return (
     <div className="component">ProductList</div>
   )
