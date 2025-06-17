298c38b2d8b7b7175b009d4cab13c3367b7d14f2
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-06-17 21:40:48 +0530

    Implement navigation and styling in Contact component

--- 07_React_Router/src/components/Contact.js
+++ 07_React_Router/src/components/Contact.js
@@ -1,5 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 export const Contact = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    console.log("----");
+    navigate("/");
+  }
+
+
   return (
+    <>
     <div className="component">Contact</div>
+    <button onClick={handleSubmit}>Back to Home</button>
+    </>
   )
 }
